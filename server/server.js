56479b4e035af650097dd6fed9194558767cf53d
bredom <domm747@gmil.com>
2022-10-02 16:13:15 +0200

    Adding cors and helmet

--- server/server.js
+++ server/server.js
@@ -9,16 +9,19 @@ const { AppError } = require('./utils/AppError');
 
 require('./db/db');
 
+const helmet = require('helmet');
+const cors = require('cors');
+
 //First install with npm
-/* const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
+/* const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean'); 
-const cookieParser = require('cookie-parser');
-const cors = require('cors'); */
+const cookieParser = require('cookie-parser'); */
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
